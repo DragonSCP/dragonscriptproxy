fe7e7f80cc80693585c4a56dcf4d470a43b715bb
DragonSCP <135724825+DragonSCP@users.noreply.github.com>
2024-04-24 12:05:36 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,4 @@ pkg install git python python-pip -y
 git clone https://github.com/DragonSCP/dragonscriptproxy.git
 cd dragonscriptproxy
 pip install -r requirements.txt
-echo "Instalação completa! Você pode agora executar o script com 'python dragon_ssh_script.py'"
+python dragon_ssh_script.py
